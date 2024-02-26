107908bc8bdd5c9a6b3a59835f4d45cca6d445f9
AbdelrhmanAmer <12121212aboziad@gmail.com>
2024-02-26 13:41:59 +0200

    InputField - themes

--- todo_app_getx/lib/screens/add_task/components/body.dart
+++ todo_app_getx/lib/screens/add_task/components/body.dart
@@ -1,10 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:todo_app_getx/constants.dart';
+import 'package:todo_app_getx/controllers/task_controller.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
   const Body({super.key});
 
   @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Container());
+    return const SafeArea(
+        child: Padding(
+      padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            InputField(
+              title: 'Title',
+              hintText: 'Enter title here',
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
+
+class InputField extends StatelessWidget {
+  const InputField({
+    super.key,
+    required this.title,
+    required this.hintText,
+    this.suffixIcon,
+    this.iconData,
+    this.controller,
+    this.values,
+  });
+  final String title, hintText;
+  final String? suffixIcon;
+  final IconData? iconData;
+  final TextEditingController? controller;
+  final List<String>? values;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(title),
+        const SizedBox(height: kDefaultPadding / 4),
+        TextFormField(
+          decoration: InputDecoration(
+            hintText: hintText,
+            hintStyle: const TextStyle(fontSize: 16),
+            suffixIcon: suffixIcon != null
+                ? Padding(
+                    padding: const EdgeInsets.all(kDefaultPadding/2),
+                    child: SvgPicture.asset(
+                      suffixIcon!,
+                      colorFilter: ColorFilter.mode(
+                          Get.theme.colorScheme.onSurface, BlendMode.srcIn),
+                    ),
+                  )
+                : iconData != null
+                    ? Icon(iconData)
+                    : null,
+          ),
+        ),
+      ],
+    );
   }
 }
--- todo_app_getx/lib/themes.dart
+++ todo_app_getx/lib/themes.dart
@@ -20,6 +20,7 @@ class Themes {
     ),
     textTheme: buildTextTheme(),
     appBarTheme: buildAppBarTheme(),
+    inputDecorationTheme: buildLightInputDecorationTheme(),
   );
 
   static final dark = ThemeData(
@@ -38,34 +39,77 @@ class Themes {
     ),
     textTheme: buildTextTheme(),
     appBarTheme: buildAppBarTheme(),
+    inputDecorationTheme: buildDarkInputDecorationTheme(),
   );
-
 }
 
 TextTheme buildTextTheme() {
   return const TextTheme().copyWith(
-    headlineLarge: GoogleFonts.lato(textStyle: const TextStyle(fontWeight: FontWeight.w600, fontSize: 30)),
-    headlineMedium:GoogleFonts.lato(textStyle: const TextStyle(fontWeight: FontWeight.w500, fontSize: 26)),
-    headlineSmall: GoogleFonts.lato(textStyle: const TextStyle(fontWeight: FontWeight.w500, fontSize: 24)),
-    titleLarge:    GoogleFonts.lato(textStyle: const TextStyle(fontWeight: FontWeight.w500, fontSize: 22)),
-    titleMedium:   GoogleFonts.lato(textStyle: const TextStyle(fontWeight: FontWeight.w500, fontSize: 20)),
-    titleSmall:    GoogleFonts.lato(textStyle: const TextStyle(fontWeight: FontWeight.w400, fontSize: 18)),
-    bodyLarge:     GoogleFonts.lato(textStyle: const TextStyle(fontWeight: FontWeight.w400, fontSize: 18)),
-    bodyMedium:    GoogleFonts.lato(textStyle: const TextStyle(fontWeight: FontWeight.w400, fontSize: 16)),
-    bodySmall:     GoogleFonts.lato(textStyle: const TextStyle(fontWeight: FontWeight.w300, fontSize: 15, height: 1.3)),
-    labelLarge:    GoogleFonts.lato(textStyle: const TextStyle(fontWeight: FontWeight.w300, fontSize: 15)),
-    labelMedium:   GoogleFonts.lato(textStyle: const TextStyle(fontWeight: FontWeight.w300, fontSize: 14)),
-    labelSmall:    GoogleFonts.lato(textStyle: const TextStyle(fontWeight: FontWeight.w200, fontSize: 12)),
+    headlineLarge: GoogleFonts.lato(
+        textStyle: const TextStyle(fontWeight: FontWeight.w600, fontSize: 30)),
+    headlineMedium: GoogleFonts.lato(
+        textStyle: const TextStyle(fontWeight: FontWeight.w500, fontSize: 26)),
+    headlineSmall: GoogleFonts.lato(
+        textStyle: const TextStyle(fontWeight: FontWeight.w500, fontSize: 24)),
+    titleLarge: GoogleFonts.lato(
+        textStyle: const TextStyle(fontWeight: FontWeight.w500, fontSize: 22)),
+    titleMedium: GoogleFonts.lato(
+        textStyle: const TextStyle(fontWeight: FontWeight.w500, fontSize: 20)),
+    titleSmall: GoogleFonts.lato(
+        textStyle: const TextStyle(fontWeight: FontWeight.w400, fontSize: 18)),
+    bodyLarge: GoogleFonts.lato(
+        textStyle: const TextStyle(fontWeight: FontWeight.w400, fontSize: 18)),
+    bodyMedium: GoogleFonts.lato(
+        textStyle: const TextStyle(fontWeight: FontWeight.w400, fontSize: 16)),
+    bodySmall: GoogleFonts.lato(
+        textStyle: const TextStyle(
+            fontWeight: FontWeight.w300, fontSize: 15, height: 1.3)),
+    labelLarge: GoogleFonts.lato(
+        textStyle: const TextStyle(fontWeight: FontWeight.w300, fontSize: 15)),
+    labelMedium: GoogleFonts.lato(
+        textStyle: const TextStyle(fontWeight: FontWeight.w300, fontSize: 14)),
+    labelSmall: GoogleFonts.lato(
+        textStyle: const TextStyle(fontWeight: FontWeight.w200, fontSize: 12)),
   );
 }
 
 AppBarTheme buildAppBarTheme() {
-  return const AppBarTheme(
+  return AppBarTheme(
     centerTitle: true,
-    titleTextStyle: TextStyle(
-      fontSize: 22,
-      fontWeight: FontWeight.w500,
-      color: kTextColor,
+    titleTextStyle: GoogleFonts.lato(
+      textStyle: const TextStyle(
+        fontSize: 22,
+        fontWeight: FontWeight.w400,
+      ),
     ),
+  );
+}
+
+InputDecorationTheme buildLightInputDecorationTheme() {
+  var outlineInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(16),
+    borderSide: const BorderSide(color: Colors.black54),
+  );
+  return InputDecorationTheme(
+    border: outlineInputBorder,
+    enabledBorder: outlineInputBorder,
+    focusedBorder: outlineInputBorder,
+    disabledBorder: outlineInputBorder,
+    contentPadding:
+        const EdgeInsets.symmetric(horizontal: kDefaultPadding, vertical: 16),
+  );
+}
+InputDecorationTheme buildDarkInputDecorationTheme() {
+  var outlineInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(16),
+    borderSide: const BorderSide(color: Colors.white54),
+  );
+  return InputDecorationTheme(
+    border: outlineInputBorder,
+    enabledBorder: outlineInputBorder,
+    focusedBorder: outlineInputBorder,
+    disabledBorder: outlineInputBorder,
+    contentPadding:
+        const EdgeInsets.symmetric(horizontal: kDefaultPadding, vertical: 16),
   );
 }
